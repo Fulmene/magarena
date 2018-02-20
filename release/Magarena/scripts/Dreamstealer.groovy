4c1264bb3c73ab47746be2ecc73b0da2993d5bcb
Ada Joule <ada.fulmina@gmail.com>
2018-02-21 01:33:22 +0700

    Add Dreamstealer

---
+++ release/Magarena/scripts/Dreamstealer.groovy
@@ -0,0 +1,19 @@
+[
+    new ThisCombatDamagePlayerTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game, final MagicPermanent permanent, final MagicDamage damage) {
+            return new MagicEvent(
+                permanent,
+                damage.getTargetPlayer(),
+                damage.getDealtAmount(),
+                this,
+                "PN discards RN cards."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.addEvent(new MagicDiscardEvent(event.getSource(), event.getPlayer(), event.getRefInt()));
+        }
+    }
+]
+
