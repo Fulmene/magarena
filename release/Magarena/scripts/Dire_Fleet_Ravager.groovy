9e8941b02a0de65babe49be4579dce29d2d61e2e
Ada Joule <ada.fulmina@gmail.com>
2018-02-21 01:27:30 +0700

    Add Dire Fleet Ravager

---
+++ release/Magarena/scripts/Dire_Fleet_Ravager.groovy
@@ -0,0 +1,21 @@
+[
+    new EntersBattleFieldTrigger() {
+        @Override
+        public MagicEvent executeTrigger(final MagicGame game,final MagicPermanent permanent, final MagicPayedCost payedCost) {
+            return new MagicEvent(
+                permanent,
+                this,
+                "Each player loses a third of his or her life, rounded up."
+            );
+        }
+        @Override
+        public void executeEvent(final MagicGame game, final MagicEvent event) {
+            game.getAPNAP().each {
+                def life = MagicAmountFactory.LifeTotal.getAmount(event.getSource(), it);
+                def amount = (life + 2).intdiv(3);
+                game.doAction(new ChangeLifeAction(it, -amount));
+            }
+        }
+    }
+]
+
